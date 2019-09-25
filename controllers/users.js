2ac87341e99ed99c645fae567679cba0a49cd6f7
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-09-25 23:34:26 +0100

    setup jwt auth

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/User')
 
@@ -90,5 +91,47 @@ exports.registerUser = (req, res) => {
                     });
                 });
             }
+        })
+}
+
+exports.loginUsers = (req, res) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: 'User not available go and Register'
+                })
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: 'Auth failed'
+                    })
+                }
+                if (result) {
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                    },
+                        'Slugterra5',
+                        {
+                            expiresIn: "1h"
+                        }
+                    );
+                    return res.status(401).json({
+                        message: 'Auth Successful',
+                        token: token
+                    })
+                }
+                return res.status(401).json({
+                    message: 'Auth failed'
+                })
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
         })
 }
--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,6 @@ router.get('/', userController.getUsers);
 router.get('/:id', userController.getUserByid);
 router.delete('/:id', userController.deleteUserById)
 router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUsers)
 
 module.exports = router
